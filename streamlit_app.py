9467f8ad56bb281fe20db04b0a3395e891874c27
Akshaj-syr <asalvi@syr.edu>
2025-09-16 23:30:37 +0000

    lok

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ hw1 = st.Page("hws/HW_1.py", title="HW1")
 hw2 = st.Page("hws/HW_2.py", title="HW2")
 hw3 = st.Page("hws/HW_3.py", title="HW3", default=True)
 
-pages = {"HWs": [hw1, hw2]}
+pages = {"HWs": [hw1, hw2, hw3]}
 
 st.navigation(pages).run()
